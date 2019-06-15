b4d5e02794ffa099ea76bc22431b96fd68fb3a35
KylerYohn <kyyohn@gmail.com>
2019-06-15 12:01:24 -0400

    creating sql models

---
+++ models/bands.js
@@ -0,0 +1,33 @@
+module.exports = function(sequelize, DataTypes) {
+  const Bands = sequelize.define("bands", {
+    bandName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    bio: DataTypes.TEXT,
+
+    //will need to json.Stringify to convert or parse it out if we are extracting
+    genres: {
+      type: DataTypes.STRING
+
+      // get: function() {
+      //   return JSON.parse(this.getDataValue("myArrayField"));
+      // },
+      // set: function(val) {
+      //   return this.setDataValue("myArrayField", JSON.stringify(val));
+      // }
+    },
+
+    email: DataTypes.STRING
+  });
+
+  // bands.associate = function(model) {
+  //   bands.hasMany(model.member, {
+  //     forgienKey: {
+  //       allowNull: false
+  //     }
+  //   });
+  // };
+  return Bands;
+};
---
+++ models/members.js
@@ -0,0 +1,22 @@
+module.exports = function(sequelize, DataTypes) {
+  const Members = sequelize.define("members", {
+    FirstName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    LastName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    bio: DataTypes.TEXT,
+
+    //will need to json.Stringify to convert or parse it out if we are extracting
+    genres: DataTypes.STRING,
+
+    experience: DataTypes.STRING,
+
+    email: DataTypes.STRING
+  });
+  return Members;
+};
