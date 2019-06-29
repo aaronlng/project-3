f609b63eb9f52ee653d8f50d2e0fd2693f83ce81
KevinW <bird831205@hotmail.com>
2019-06-28 21:02:25 -0400

    190628 styling

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -10,6 +10,8 @@ class Home extends Component {
   state = {
     searchQuery: "",
     queryType: "Band by Name",
+    searchedBand: false,
+    searchedMember: false,
     bandResult: [],
     memberResult: []
   };
@@ -44,6 +46,12 @@ class Home extends Component {
     // this.loadMembe rs();
     switch (this.state.queryType) {
       case "Band by Name":
+        // changing search rendering state
+        console.log()
+        this.setState({
+          searchedBand: true,
+          searchedMember: false
+        })
         if (this.state.searchQuery) {
           API.getBandsByName(this.state.searchQuery)
             .then(res => {
@@ -52,9 +60,20 @@ class Home extends Component {
             })
             .catch(err => console.log(err));
           this.setState({ searchQuery: "" });
+        } else {
+          console.log("get all bands")
+          API.getBands()
+            .then(res => {
+              console.log(res.data);
+              this.setState({ bandResult: res.data, memberResult: [] })
+            })
         }
         break;
       case "Band by Genre":
+        this.setState({
+          searchedBand: true,
+          searchedMember: false
+        })
         if (this.state.searchQuery) {
           API.getBandsByGenre(this.state.searchQuery)
             .then(res => {
@@ -64,9 +83,19 @@ class Home extends Component {
             .catch(err => console.log(err));
 
           this.setState({ searchQuery: "" });
+        } else {
+          API.getBands()
+            .then(res => {
+              console.log(res.data);
+              this.setState({ bandResult: res.data, memberResult: [] })
+            })
         }
         break;
       case "Member by Name":
+        this.setState({
+          searchedBand: false,
+          searchedMember: true
+        })
         if (this.state.searchQuery) {
           API.getMembersByName(this.state.searchQuery)
             .then(res => {
@@ -76,9 +105,18 @@ class Home extends Component {
             .catch(err => console.log(err));
 
           this.setState({ searchQuery: "" });
+        } else {
+          API.getMembers()
+            .then(res => {
+              this.setState({ memberResult: res.data, bandResult: [] })
+            })
         }
         break;
       case "Member by Genre":
+        this.setState({
+          searchedBand: false,
+          searchedMember: true
+        })
         if (this.state.searchQuery) {
           API.getMembersByGenre(this.state.searchQuery)
             .then(res => {
@@ -88,6 +126,11 @@ class Home extends Component {
             .catch(err => console.log(err));
 
           this.setState({ searchQuery: "" });
+        } else {
+          API.getMembers()
+            .then(res => {
+              this.setState({ memberResult: res.data, bandResult: [] })
+            })
         }
         break;
       default:
@@ -96,7 +139,7 @@ class Home extends Component {
 
   render() {
     return (
-      <div className="container">
+      <Container className="page-container">
         <h1>Bandly</h1>
         <Row>
           {this.state.featuredMember ? (
@@ -106,10 +149,10 @@ class Home extends Component {
               <a href={`/member/${this.state.featuredMember.id}`}>Link</a>
             </Col>
           ) : (
-            <Col s={12} m={6}>
-              <h3>No Featured Member yet become one today </h3>
-            </Col>
-          )}
+              <Col s={12} m={6}>
+                <h3>No Featured Member yet become one today </h3>
+              </Col>
+            )}
           {this.state.featuredBand ? (
             <Col s={12} m={6}>
               <h3>Featured Band</h3>
@@ -117,17 +160,17 @@ class Home extends Component {
               <a href={`/band/${this.state.featuredBand.id}`}>Link</a>
             </Col>
           ) : (
-            <Col s={12} m={6}>
-              <h3>No Featured Band yet become one today </h3>
-            </Col>
-          )}
+              <Col s={12} m={6}>
+                <h3>No Featured Band yet become one today </h3>
+              </Col>
+            )}
         </Row>
 
         <div>
-          <h2>Find more bands and members</h2>
+          <h2>Find More Bands and Members</h2>
 
           <div className="row">
-            <div className="col s3">
+            <div className="col s2">
               <SearchSelect onChange={this.handleInputChange} name="queryType">
                 <option>Band by Name</option>
                 <option>Band by Genre</option>
@@ -135,9 +178,10 @@ class Home extends Component {
                 <option>Member by Genre</option>
               </SearchSelect>
             </div>
-            <div className="col s7">
+            <div className="col s8">
               <SearchInput
                 onChange={this.handleInputChange}
+                value={this.state.searchQuery}
                 name="searchQuery"
                 className="search-input"
                 placeholder="Search for a Band or Band Member"
@@ -149,31 +193,73 @@ class Home extends Component {
               </SearchBtn>
             </div>
           </div>
+
           <Container className="searchResult">
-            {this.state.bandResult.map(result => {
-              const link = "/band/" + result.id;
-              return (
-                <Row key="result">
-                  <Col s={12}>
-                    <Card horizontal className="post">
-                      <Row>
-                        <Col className="card-image" s={3}>
-                          {/* <img src={img} className="post-image"></img> */}
-                        </Col>
-                        <Col s={9}>
-                          <h1 className="card-title">{result.bandName}</h1>
-                          <p className="card-content">{result.bio}</p>
-                          <a href={link}>link</a>
+
+            {this.state.searchedBand && (
+              this.state.bandResult[0] ? (
+                this.state.bandResult.map(result => {
+                  const link = "/band/" + result.id;
+                  return (
+                    <Row key="result">
+                      <Col s={12}>
+                        <Card horizontal className="post">
+                          <Row>
+                            <Col className="card-image" s={3}>
+                              {/* <img src={img} className="post-image"></img> */}
+                            </Col>
+                            <Col s={9}>
+                              <h1 className="card-title">{result.bandName}</h1>
+                              <p className="card-content">{result.bio}</p>
+                              <a href={link}>link</a>
+                            </Col>
+                          </Row>
+                        </Card>
+                      </Col>
+                    </Row>
+                  );
+                })
+              ) : (
+                  <h3>No Results Found</h3>
+                )
+            )}
+
+
+            {
+              this.state.searchedMember && (
+                this.state.memberResult[0] ? (
+                  this.state.memberResult.map(result => {
+                    const link = "/member/" + result.id;
+                    return (
+                      <Row key="result">
+                        <Col s={12}>
+                          <Card horizontal className="post">
+                            <Row>
+                              <Col className="card-image" s={3}>
+                                {/* <img src={img} className="post-image"></img> */}
+                              </Col>
+                              <Col s={9}>
+                                <h1 className="card-title">{result.fullName}</h1>
+                                <p className="card-content">{result.bio}</p>
+                                <a href={link}>link</a>
+                              </Col>
+                            </Row>
+                          </Card>
                         </Col>
                       </Row>
-                    </Card>
-                  </Col>
-                </Row>
-              );
-            })}
+                    );
+                  })
+                ) : (
+                    <h1>No Results Found</h1>
+                  )
+              )
+
+
+            }
+
           </Container>
         </div>
-      </div>
+      </Container>
     );
   }
 }
